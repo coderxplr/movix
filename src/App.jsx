486d341a909fbec286686998408e98f8e7e3198d
AwaizMd <mdawaiz08@outlook.com>
2024-01-25 15:32:50 +0530

    created redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log("state: ", url);
   useEffect(() => {
     apiTesting();
   }, []);
@@ -9,12 +14,14 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log("res: ", res);
+      dispatch(getApiConfiguration(res));
     });
   };
 
   return (
     <div className="App">
-      {/* {fetchDataFromApi("/genre/movie/list?language=en")} */}
+      APP
+      {url?.total_pages}
     </div>
   );
 }
